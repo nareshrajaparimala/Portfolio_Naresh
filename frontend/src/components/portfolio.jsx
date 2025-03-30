75bb7aabccd7ff341c7fe711ae3c53f7a9863f73
Naresh.R <nareshrajaparimala000@gmail.com>
2025-03-30 11:47:09 +0530

    i have written js in react the swap of porjects , certificate , and techstack .

--- frontend/src/components/portfolio.jsx
+++ frontend/src/components/portfolio.jsx
@@ -1,6 +1,87 @@
-import React from "react";
+import { useRef} from "react";
 import './styles/portfolio.css'
 function Portfolio(){
+    // project handle
+    const projectdiv=useRef(null);
+    const certicatediv=useRef(null);
+    const techdiv=useRef(null);
+    // button
+    const projectbuthandle=useRef(null);
+    const certificatebuthandle=useRef(null);
+    const techbuthandle=useRef(null);
+
+    const counthead=1;
+    // hover function
+    const handleMouseEnter1 = () => {
+        projectbuthandle.current.style.background="linear-gradient(to right, purple, darkblue)";
+    }
+    const handleMouseLeave =() =>{
+        if( projectdiv.current.style.display == "none"){
+            projectbuthandle.current.style.background="none";
+        }
+        if( certicatediv.current.style.display == "none"){
+            certificatebuthandle.current.style.background="none";
+        }
+        if( techdiv.current.style.display == "none"){
+            techbuthandle.current.style.background="none";
+        }
+        if( projectdiv.current.style.display == "flex"){
+            projectbuthandle.current.style.background="flex";
+        }
+        if( certicatediv.current.style.display == "flex"){
+            certificatebuthandle.current.style.background="flex";
+        }
+        if( techdiv.current.style.display == "flex"){
+            techbuthandle.current.style.background="flex";
+        }
+    }
+    const handleMouseEnter2 = () => {
+        certificatebuthandle.current.style.background="linear-gradient(to right, purple, darkblue)";
+        
+    }
+    const handleMouseEnter3 = () => {
+        techbuthandle.current.style.background="linear-gradient(to right, purple, darkblue)";
+    }
+
+
+
+    const projecthandle =()=>{
+        if(projectdiv.current && certicatediv.current && techdiv.current && projectbuthandle.current && certificatebuthandle.current && certificatebuthandle.current){
+            projectdiv.current.style.display="flex";
+            certicatediv.current.style.display="none";
+            techdiv.current.style.display="none";
+            // button
+            projectbuthandle.current.style.background="linear-gradient(to right, purple, darkblue)";
+            certificatebuthandle.current.style.background="transparent";
+            techbuthandle.current.style.background="transparent"
+            counthead=1;
+        }
+    }
+    const certificatehandle=()=>{
+        if(projectdiv.current && certicatediv.current && techdiv.current && projectbuthandle.current && certificatebuthandle.current && certificatebuthandle.current){
+            projectdiv.current.style.display="none";
+            certicatediv.current.style.display="flex";
+            techdiv.current.style.display="none";
+            // button
+            projectbuthandle.current.style.background="transparent";
+            certificatebuthandle.current.style.background="linear-gradient(to right, purple, darkblue)";
+            techbuthandle.current.style.background="transparent"
+            counthead=2;
+        }
+    }
+    const techhandle=()=>{
+        if(projectdiv.current && certicatediv.current && techdiv.current && projectbuthandle.current && certificatebuthandle.current && certificatebuthandle.current){
+            projectdiv.current.style.display="none";
+            certicatediv.current.style.display="none";
+            techdiv.current.style.display="flex";
+            // button
+            projectbuthandle.current.style.background="transparent";
+            certificatebuthandle.current.style.background="transparent";
+            techbuthandle.current.style.background="linear-gradient(to right, purple, darkblue)";
+            counthead=3;
+        }
+    }
+
     return (
         <portfolio>
             <div className="portheaddiv">
@@ -9,15 +90,18 @@ function Portfolio(){
             </div>
             {/* 3 parts  */}
             <div className="portdivider">
-                <div className="projectsbut boxdez">
+                <div className="projectsbut boxdez" onClick={projecthandle} ref={projectbuthandle} onMouseEnter={handleMouseEnter1}
+      onMouseLeave={handleMouseLeave}>
                 <i class="ri-code-line"></i>
                     Projects
                 </div>
-                <div className="certificates boxdez">
+                <div className="certificates boxdez" onClick={certificatehandle} ref={certificatebuthandle} onMouseEnter={handleMouseEnter2}
+      onMouseLeave={handleMouseLeave}>
                 <i class="ri-medal-line"></i>
                     Certificates
                 </div>
-                <div className="techStack boxdez">
+                <div className="techStack boxdez" onClick={techhandle} ref={techbuthandle} onMouseEnter={handleMouseEnter3}
+      onMouseLeave={handleMouseLeave}>
                 <i class="ri-color-filter-ai-fill"></i>
                     Tech Stack
                 </div>
@@ -25,7 +109,7 @@ function Portfolio(){
             {/* all 3 parts handling */}
 
             {/* start of 1st part projects */}
-            <div className="projectsbigdiv">
+            <div className="projectsbigdiv" ref={projectdiv}>
                 <div className="containerpro">
                     <div className="proimg">
                     </div>
@@ -114,7 +198,7 @@ function Portfolio(){
                 </div>
             </div>
             {/* certificates */}
-            <div className="certificatesection">
+            <div className="certificatesection" ref={certicatediv}>
                 {/* certificate-1 */}
                 <div className="containercertificate">
                     <div className="certificateimage">
@@ -142,7 +226,7 @@ function Portfolio(){
             </div>
 
             {/* tech stack */}
-            <div className="techstackdiv">
+            <div className="techstackdiv" ref={techdiv}>
                 <div className="techstacksub">
                     <div className="logoskill img1"></div>
                     <h3>javaScript</h3>
@@ -175,7 +259,9 @@ function Portfolio(){
                 </div>
                 <div className="techstacksub">
                     <div className="logoskill img8"></div>
-                    <h3>javaScript</h3>
+                    <h3>
+                        c
+                    </h3>
                 </div>
             </div>
 
